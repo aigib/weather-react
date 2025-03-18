482c2763422959e4dc12a3d598e1e19beb758df1
aigib <aigibuht@gmail.com>
2025-03-18 12:30:29 +0200

    Added axios integration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import AtomicSpinner from "atomic-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "7746bdeabca928cfedcad71e52fd9d66";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <AtomicSpinner
+      atomSize={200}
+      displayElectronPaths="false"
+      electronPathColor="#707070"
+      electronSize={1.5}
+      electronSpeed={0.5}
+      nucleusDistanceFromCenter={2.5}
+    />
+  );
+}
